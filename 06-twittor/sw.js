881377fc90fb6cadfd645a9b241db84f17ddeca7
lgelarci <luis.galarcio@MacBook-Pro-de-administrador-4.local>
2019-01-14 10:19:10 -0500

    test

--- 06-twittor/sw.js
+++ 06-twittor/sw.js
@@ -7,7 +7,7 @@ const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL       = [
-    '/', // No va a servir en producción
+    // '/', // No va a servir en producción
     'index.html',
     'css/style.css',
     'img/favicon.ico',
